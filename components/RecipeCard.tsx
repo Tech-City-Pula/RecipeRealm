6457612ef6bd6ff7fcefeb7711004a563b2becad
alanbubalo <75133040+alanbubalo@users.noreply.github.com>
2024-04-11 01:55:14 +0200

    recipe-gallery

---
+++ components/RecipeCard.tsx
@@ -0,0 +1,39 @@
+"use server"
+
+import Image from 'next/image';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+
+interface Recipe {
+	image: {
+		url: string;
+		name: string;
+	} | undefined;
+	created_at: string;
+	description: string | null;
+	id: number;
+	ingredients: string | null;
+	methods: string | null;
+	name: string;
+}
+
+export default async function RecipeCard({ recipe }: { recipe: Recipe }) {
+	return (
+		<Card>
+			<CardHeader>
+				<Image width={500} height={500} className="w-full h-64 rounded object-center object-cover" src={recipe.image?.url ?? ''} alt={recipe.image?.name ?? ''}/>
+				<h1 className="text-2xl font-bold">{recipe.name}</h1>
+				<p>{recipe.description}</p>
+			</CardHeader>
+			<CardContent className="space-y-2">
+				<div>
+					<h2 className="font-semibold">Ingredients</h2>
+					<p>{recipe.ingredients}</p>
+				</div>
+				<div>
+					<h2 className="font-semibold">Methods</h2>
+					<p>{recipe.methods}</p>
+				</div>
+			</CardContent>
+		</Card>
+	)
+}
---
+++ components/RecipeGallery.tsx
@@ -0,0 +1,55 @@
+"use server"
+
+import { createClient } from "@/lib/supabase/server";
+import { Tables } from "@/db-types";
+import RecipeCard from "@/components/RecipeCard";
+
+async function getFirstImageInFolder(folderName: string) {
+		const supabase = createClient();
+    try {
+        const { data: files, error } = await supabase.storage
+            .from('recipe-images')
+            .list(folderName, { limit: 1 });
+        if (error) throw error;
+
+				const firstFile = files[0];
+				const { data } = supabase.storage.from('recipe-images').getPublicUrl(`${folderName}/${firstFile.name}`);
+
+        return {
+					url: data.publicUrl,
+					name: firstFile.name
+				};
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default async function RecipeGallery() {
+	const supabase = createClient();
+
+	const { data: recipesContents = [], error } = await supabase.from("recipe").select("*").returns<Tables<'recipe'>[]>();
+	if (error) {
+		console.error(error);
+	}
+
+	const recipes = await Promise.all(recipesContents?.map(async recipe => {
+		const image = await getFirstImageInFolder(recipe.id.toString());
+		return {
+			...recipe,
+			image
+		};
+	}) ?? []);
+
+	return (
+		<div>
+			{ recipes?.length === 0 && <p className="mx-auto w-fit pt-16">No recipes found</p>}
+			<section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 md:p-6">
+				{recipes?.map((recipe) => (
+					<div key={recipe.id}>
+						<RecipeCard recipe={recipe}/>
+					</div>
+				))}
+			</section>
+		</div>
+	);
+}
